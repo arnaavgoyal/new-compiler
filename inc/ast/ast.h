94ac9bd305dffff512e3b18fe00ad180b5742468
arnaavgoyal <58274830+arnaavgoyal@users.noreply.github.com>
2023-12-23 07:56:07 -0500

    progress on ast refactor

--- inc/ast/ast.h
+++ inc/ast/ast.h
@@ -143,8 +143,37 @@ public:
 
 enum class stmtkind {
 
+    ret,
+    brk,
+    cont,
+    stmt_block,
+    loop,
+    ifthen,
+
     // typed statements
     _typed_stmt_start,
+
+        // expressions
+        _expr_start,
+
+            ref,
+            call,
+            subscript,
+            paren,
+            cast,
+            unop,
+            binop,
+
+            // literals
+            _literal_start,
+
+                integer,
+                character,
+                string,
+
+            _literal_end,
+
+        _expr_end,
 
         // declarations
         _decl_start,
@@ -163,38 +192,9 @@ enum class stmtkind {
 
         _decl_end,
 
-        // literals
-        _literal_start,
-
-            integer,
-            character,
-            string,
-
-        _literal_end,
-
-        // expressions
-        _expr_start,
-
-            loop,
-            ifthen,
-            reference,
-            call,
-            subscript,
-            parenthesized,
-            cast,
-            unop,
-            binop,
-
-        _expr_end,
-
     _typed_stmt_end,
 
-    return_stmt,
-    break_stmt,
-    continue_stmt,
-
     // special
-    stmt_block,
     error
 };
 
@@ -203,104 +203,35 @@ enum class valuekind {
     rvalue
 };
 
-class Stmt {
-private:
-    stmtkind kind;
-    SourceLocation loc;
-    unsigned num;
-
-protected:
-    std::vector<Stmt *> children;
-
-    Stmt(stmtkind kind, SourceLocation loc)
-        : kind(kind), loc(loc) { }
-    
-
-public:
-    stmtkind get_kind() { return kind; }
-    SourceLocation get_loc() { return loc; }
-};
-
-/**
- * This class only exists to factor out common type logic
- * from Decl and Expr.
-*/
-class TypedStmt : public Stmt {
-private:
-    Type *type;
-
-protected:
-    TypedStmt(stmtkind kind, SourceLocation loc, Type *type)
-        : Stmt(kind, loc), type(type) { }
-
-public:
-    Type *get_type() { return type; }
-};
-
-/**
- * A language declaration.
- * These can appear in any context.
-*/
-class Decl : public TypedStmt {
-private:
-    Symbol *symbol;
-
-protected:
-    Decl(stmtkind kind, SourceLocation loc, Type *type, Symbol *symbol)
-        : TypedStmt(kind, loc, type), symbol(symbol) { }
-
-public:
-    Symbol *get_symbol() { return symbol; }
-};
-
-/**
- * A language expression.
- * An expression is every language construct other than declarations.
-*/
-class Expr : public TypedStmt {
-private:
-    valuekind vk;
-
-protected:
-    Expr(stmtkind kind, SourceLocation loc, Type *type, valuekind valkind)
-        : TypedStmt(kind, loc, type), vk(valkind) { }
-
-public:
-    valuekind get_valuekind() { return vk; }
-};
-
-class TypeDecl : public Decl {
-protected:
-    TypeDecl(SourceLocation loc, Type *type, Symbol *symbol)
-        : Decl(stmtkind::type, loc, type, symbol) { }
-};
-
-class VarDecl : public Decl {
-protected:
-    VarDecl(stmtkind kind, SourceLocation loc, Type *type, Symbol *symbol)
-        : Decl(kind, loc, type, symbol) { }
-
-};
-
-class LocalVarDecl : public Decl {
-protected:
-    LocalVarDecl(SourceLocation loc, Type *type, Symbol *symbol)
-        : Decl(stmtkind::localvar, loc, type, symbol) { }
-
-};
-
-class GlobalVarDecl : public Decl {
-protected:
-    GlobalVarDecl(SourceLocation loc, Type *type, Symbol *symbol)
-        : Decl(stmtkind::globalvar, loc, type, symbol) { }
-
-};
-
-class ParamDecl : public VarDecl {
-protected:
-    ParamDecl(SourceLocation loc, Type *type, Symbol *symbol)
-        : VarDecl(stmtkind::parameter, loc, type, symbol) { }
-};
+class DeclContext;
+class ExecContext;
+class Stmt;
+class RetStmt;
+class BreakStmt;
+class ContinueStmt;
+class StmtBlock;
+class LoopStmt;
+class IfThenStmt;
+class TypedStmt;
+class Expr;
+class RefExpr;
+class CallExpr;
+class SubscriptExpr;
+class ParenExpr;
+class CastExpr;
+class UnOpExpr;
+class BinOpExpr;
+class Literal;
+class IntLiteral;
+class CharLiteral;
+class StringLiteral;
+class Decl;
+class VarDecl;
+class LocalVarDecl;
+class GlobalVarDecl;
+class ParamDecl;
+class TypeDecl;
+class FuncDecl;
 
 /**
  * A declaration context. Only declarations may exist within this context.
@@ -316,10 +247,214 @@ class ExecContext : public DeclContext {
 
 };
 
+/** ------------- Statements -------------- */
+
+class Stmt {
+private:
+    stmtkind kind;
+
+protected:
+    Stmt(stmtkind kind) : kind(kind) { }
+    
+public:
+    stmtkind get_kind() { return kind; }
+    virtual SourceLocation start_loc() = 0;
+    virtual SourceLocation end_loc()   = 0;
+};
+
+class RetStmt : public Stmt {
+private:
+    Expr *expr;
+    SourceLocation loc;
+
+protected:
+    RetStmt(Expr *e, SourceLocation loc) : Stmt(stmtkind::ret), expr(e), loc(loc) { }
+
+public:
+    SourceLocation start_loc() override { return loc; }
+    SourceLocation end_loc()   override { return loc; }
+};
+
+class BreakStmt : public Stmt {
+private:
+    SourceLocation loc;
+
+protected:
+    BreakStmt(Expr *e, SourceLocation loc) : Stmt(stmtkind::ret), loc(loc) { }
+
+public:
+    SourceLocation start_loc() override { return loc; }
+    SourceLocation end_loc()   override { return loc; }
+};
+
+class ContinueStmt : public Stmt {
+private:
+    SourceLocation loc;
+
+protected:
+    ContinueStmt(Expr *e, SourceLocation loc) : Stmt(stmtkind::ret), loc(loc) { }
+
+public:
+    SourceLocation start_loc() override { return loc; }
+    SourceLocation end_loc()   override { return loc; }
+};
+
+class StmtBlock : public Stmt {
+private:
+    std::vector<Stmt *> stmts;
+    SourceLocation lbloc, rbloc;
+
+protected:
+    StmtBlock(std::vector<Stmt *> stmts, SourceLocation lloc, SourceLocation rloc)
+        : Stmt(stmtkind::stmt_block), stmts(stmts), lbloc(lloc), rbloc(rloc) { }
+
+public:
+    SourceLocation start_loc() override { return lbloc; }
+    SourceLocation end_loc()   override { return rbloc; }
+};
+
+class LoopStmt : public Stmt {
+private:
+    SourceLocation kwloc, lploc, rploc;
+
+protected:
+    LoopStmt(Expr *cond, StmtBlock *body, SourceLocation kwloc, SourceLocation lploc, SourceLocation rploc)
+        : Stmt(stmtkind::loop), kwloc(kwloc), lploc(lploc), rploc(rploc) { }
+
+public:
+    SourceLocation start_loc() override { return kwloc; }
+    SourceLocation end_loc()   override { return rploc; }
+};
+
+class IfThenStmt : public Stmt {
+private:
+    SourceLocation kwloc, lploc, rploc;
+
+protected:
+    IfThenStmt(Expr *cond, StmtBlock *body, SourceLocation kwloc, SourceLocation lploc, SourceLocation rploc)
+        : Stmt(stmtkind::loop), kwloc(kwloc), lploc(lploc), rploc(rploc) { }
+
+public:
+    SourceLocation start_loc() override { return kwloc; }
+    SourceLocation end_loc()   override { return rploc; }
+};
+
+/**
+ * This class only exists to factor out common type logic
+ * from Decl and Expr.
+*/
+class TypedStmt : public Stmt {
+private:
+    Type *type;
+
+protected:
+    TypedStmt(stmtkind kind, Type *type)
+        : Stmt(kind), type(type) { }
+
+public:
+    Type *get_type() { return type; }
+};
+
+/** ------------- Expressions -------------- */
+
+/**
+ * A language expression.
+ * An expression is every language construct other than declarations.
+*/
+class Expr : public TypedStmt {
+private:
+    valuekind vk;
+
+protected:
+    Expr(stmtkind kind, Type *type, valuekind valkind)
+        : TypedStmt(kind, type), vk(valkind) { }
+
+public:
+    valuekind get_valuekind() { return vk; }
+};
+
+class RefExpr : public Expr {
+private:
+    Decl *ref;
+};
+
+class CallExpr : public Expr { };
+
+class SubscriptExpr : public Expr { };
+
+class ParenExpr : public Expr { };
+
+class CastExpr : public Expr { };
+
+class UnOpExpr : public Expr { };
+
+class BinOpExpr : public Expr { };
+
+/** ------------- Literals -------------- */
+
+class Literal : public Expr { };
+
+class IntLiteral : public Literal { };
+
+class CharLiteral : public Literal { };
+
+class StringLiteral : public Literal { };
+
+/** ------------- Declarations -------------- */
+
+/**
+ * A language declaration.
+ * These can appear in any context.
+*/
+class Decl : public TypedStmt {
+private:
+    Symbol *symbol;
+
+protected:
+    Decl(stmtkind kind, Type *type, Symbol *symbol)
+        : TypedStmt(kind, type), symbol(symbol) { }
+
+public:
+    Symbol *get_symbol() { return symbol; }
+};
+
+class VarDecl : public Decl {
+protected:
+    VarDecl(stmtkind kind, Type *type, Symbol *symbol)
+        : Decl(kind, type, symbol) { }
+
+};
+
+class LocalVarDecl : public VarDecl {
+protected:
+    LocalVarDecl(Type *type, Symbol *symbol)
+        : VarDecl(stmtkind::localvar, type, symbol) { }
+
+};
+
+class GlobalVarDecl : public VarDecl {
+protected:
+    GlobalVarDecl(Type *type, Symbol *symbol)
+        : VarDecl(stmtkind::globalvar, type, symbol) { }
+
+};
+
+class ParamDecl : public VarDecl {
+protected:
+    ParamDecl(Type *type, Symbol *symbol)
+        : VarDecl(stmtkind::parameter, type, symbol) { }
+};
+
+class TypeDecl : public Decl {
+protected:
+    TypeDecl(Type *type, Symbol *symbol)
+        : Decl(stmtkind::type, type, symbol) { }
+};
+
 class FuncDecl : public Decl, public ExecContext {
 protected:
-    FuncDecl(SourceLocation loc, Type *type, Symbol *symbol)
-        : Decl(stmtkind::function, loc, type, symbol), ExecContext() { }
+    FuncDecl(Type *type, Symbol *symbol)
+        : Decl(stmtkind::function, type, symbol), ExecContext() { }
 };
 
 class TranslationUnit : public DeclContext {
